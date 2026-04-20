3661864da1fd2f2c03e342f2da5b57514d245f6d
akira koga <whimhatsu@gmail.com>
2026-04-20 20:50:20 +0900

    test: test iframe api

---
+++ source/youtube_api.js
@@ -0,0 +1,39 @@
+function playYouTube(video_id, start_sec, end_sec) {
+  const script_tag = document.createElement('script');
+  script_tag.src = 'https://www.youtube.com/iframe_api';
+  document.body.appendChild(script_tag);
+  let player;
+
+  // iframe のセットアップ
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', { //div id
+      videoId: video_id, // 動画ID
+      width: 1280, // プレーヤーの幅
+      height: 720, // プレーヤーの高さ
+      playerVars: {
+        'autoplay'  : 1, // 自動再生
+        'mute'      : 1,
+        'controls'  : 0, // コントロールの表示/非表示
+        'loop'      : 0, // 繰り返し再生のオン/オフ
+        'start'     : start_sec, //開始時間
+        'end'       : end_sec //終了時間
+      },
+      events: {
+        'onStateChange': onPlayerStateChange, // 再生状態の変化を検知
+        'onPlaybackRateChange': onPlayerPlaybackRateChange, // 再生速度の変化を検知
+        'onError': onPlayerError // エラーの発生を検知
+      }
+    });
+    function onPlayerStateChange(event) {
+      if (event.data == YT.PlayerState.PLAYING) {
+        player.unMute();
+        const currentTime = player.getCurrentTime(); // 再生位置を取得
+        if (currentTime >= end_sec) {
+          //stop and play next
+          player.pauseVideo
+        }
+    }
+  }
+
+
+}
